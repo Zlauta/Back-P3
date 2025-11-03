081ecc46aada04bc98a7771b1aca42f2f1c72016
Amelia A <maantonib@yahoo.com.ar>
2025-11-03 20:27:29 -0300

    conexión entre el frontend y el backend mediante Axios

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { connectDB } from "./src/config/config.db.js";
-
+import routes from "./src/routes/index.routes.js";
 
 connectDB();
 
 const app = express();
 const PORT = 3000;
+
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log("Servidor corriendo en el puerto 3000");
