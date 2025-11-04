8ccba2c545cb06df13bfde306577948d96dd2fb6
Zlauta <Zlauuta23@gmail.com>
2025-11-04 08:20:55 -0300

    feat(services-products): add get product by id service

--- src/services/productos.service.js
+++ src/services/productos.service.js
@@ -15,3 +15,28 @@ try {
 }
 
 }
+
+export const obtenerProductoPorIdService = async (id) => {
+try {
+    const producto = await ProductoModel.findById(id);
+    if (!producto) {
+        return {
+            msg: "Producto no encontrado",
+            statusCode: 404,
+            data: null,
+        };
+    }
+    return {
+        msg: "Producto obtenido exitosamente",
+        statusCode: 200,
+        data: producto,
+    };
+} catch (error) {
+    return {
+        msg: "Error al obtener producto",
+        statusCode: 500,
+        data: null,
+    };
+}
+}
+
