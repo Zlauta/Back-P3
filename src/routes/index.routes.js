9290b2ba98ae6bda09d029fde8460c8c575ce54c
Zlauta <Zlauuta23@gmail.com>
2025-11-20 09:13:55 -0300

    feat(contacto): add contacto routes for create and manage contact entries

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
 import usuariosRoutes from "./usuarios.routes.js";
-
 import productosRoutes from "./productos.routes.js";
+import contactoRoutes from "./contacto.routes.js"; 
 
 const router = Router();
 
 router.use("/usuarios", usuariosRoutes);
 
 router.use("/productos", productosRoutes);
+
+router.use("/contacto", contactoRoutes);
 
 export default router;
