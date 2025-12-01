ae798809c312e41e3bf53cd496295dc6299df6c0
Zlauta <Zlauuta23@gmail.com>
2025-12-01 01:52:01 -0300

    Fix: Config port dinámic for Render and cleaner index.js

--- index.js
+++ index.js
@@ -12,16 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-
-//app.use(cors());
-app.use(express.json());
-//app.use(morgan("dev"));
 
 
 app.use("/api", routes);
 
 app.listen(PORT, () => {
-  console.log("Servidor corriendo en el puerto 3000");
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
